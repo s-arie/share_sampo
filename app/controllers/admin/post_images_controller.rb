a68465d090c4ff6bc53a33dd757f423017882828
EC2 Default User <ec2-user@ip-172-31-8-173.ap-northeast-1.compute.internal>
2022-11-20 03:10:10 +0000

    [Add]ページネーション

---
+++ app/controllers/admin/post_images_controller.rb
@@ -0,0 +1,35 @@
+class Admin::PostImagesController < ApplicationController
+  def index
+    @post_images = PostImage.all
+  end
+
+  def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @post_image.update(post_image_params)
+      redirect_to post_image_path(@post_image)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+  end
+
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:title, :image, :caption)
+  end
+
+end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,12 +4,12 @@ class PostImage < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  def get_image
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/default-image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+    image.variant(resize_to_limit: [width, height]).processed
   end
 
   def favorited_by?(user)
