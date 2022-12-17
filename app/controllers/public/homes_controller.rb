5594f8a532debea0c75dd91b655e7ee7a83a3261
EC2 Default User <v005ab.nichiasa@gmail.com>
2022-12-17 12:45:31 +0000

    Swiper導入

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    post_image_ranking_ids = Favorite.select('post_image_id, count(id) as count').group('post_image_id').order('count DESC').limit(5).map {|favorite| favorite.post_image_id}
+    @post_images = PostImage.find(post_image_ranking_ids)
   end
 
   def about
